41a9fac1610440e2ee04af223b32f7e29392c1c3
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-07-15 11:22:39 +0300

    remove redundant rename

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export * from './types.js';
 export * from './validate/index.js';
 
-export { superAPI as superAPI } from './server.js';
+export { superAPI } from './server.js';
 export { superActions } from './client.js';
