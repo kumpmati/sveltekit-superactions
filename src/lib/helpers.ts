ee295bfd5908ec6e0875f175bc2654febc9d4f8b
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2024-07-20 10:53:43 +0300

    add comment to helper function

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,3 +1,8 @@
+/**
+ * Helper function that creates a new object with the same keys but different values
+ * @param val Original object
+ * @param map Function that returns a value for each key
+ */
 export const mapKeys = <T extends Record<string, unknown>, V>(
 	val: T,
 	map: (key: keyof T) => V
